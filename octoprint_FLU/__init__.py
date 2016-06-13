ab9f0b757652c86c245b929900996c81600baea3
Dave Robinson <robboz4@gmail.com>
2016-06-13 11:53:06 -0700

    Adding Files
    Adding files to complete the plugin distribution via OctoPrint plugin
    manager

---
+++ octoprint_FLU/__init__.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class FLUPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        self._logger.info("You have FLU!")
+
+
+
+
+__plugin_name__ = "FLU"
+__plugin_version__ = "0.0.1"
+__plugin_description__ = " Filament Load and Unload Plugin"
+__plugin_implementation__ = FLUPlugin()
